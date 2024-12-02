de6a858442ef2efb1a24c3bc0ecdd04e1823446f
HelenaAlamp <159056949+HelenaAlamp@users.noreply.github.com>
2024-12-02 09:53:30 -0500

    Update elements.jl

--- EPIC.jl-master/src/lattice/elements.jl
+++ EPIC.jl-master/src/lattice/elements.jl
@@ -7,7 +7,7 @@ include("wakefield.jl")
 include("strongbb_new.jl")
 include("radiation.jl")
 
-include("SC_BasettiErskine_model.jl") #added for SC calculations
+#include("SC_BasettiErskine_model.jl") #added for SC calculations
 include("Symp_SC_BE_model.jl") #added for  SC calculation, Symplectic map
 
 include("SC_sympl_gridless.jl") #added for SC symplect. gridless

83b92a90b1ee366800f51944edd9b9ddf1fe9d36
HelenaAlamp <159056949+HelenaAlamp@users.noreply.github.com>
2025-03-27 13:07:53 -0400

    Update EPIC.jl

--- EPIC.jl-master/src/EPIC.jl
+++ EPIC.jl-master/src/EPIC.jl
@@ -21,7 +21,7 @@ export optics4DUC
 export StrongGaussianBeam, StrongThinGaussianBeam, initilize_zslice!, Bassetti_Erskine!, crab_crossing_setup!, get_tune_spread!
 
 #export SC_lens, SC_kick!
-export SC_lens, parameters_derivatives, SC_kick!
+export SC_lens, parameters_derivatives, SC_kick!, sc_in_element
 
 export Drift, ThinCorrector
 export TransferMap4D, Inverse_TransferMap4D, TransferMap4DChrom, LongitudinalRFMap, OneTurnRadiation
